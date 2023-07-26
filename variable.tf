ca886b3fa46d483fedb164c36e4ca66663dd524e
cumberland-cloud <the.cumberland.cloud@gmail.com>
2023-07-26 14:01:05 -0400

    add var

--- variable.tf
+++ variable.tf
@@ -1,9 +1,10 @@
 variable "repository" {
-    description         = "ECR configuration object"
+    description         = "ECR configuration object. Policy should be a JSON string and will default to allowing all users in the caller account READ/WRITE access to the ECR."
     type                = object({
         namespace       = string
         name            = string
         kms_key_arn     = optional(string, null)
         mutability      = optional(string, "MUTABLE")
+        policy          = optional(string, null)
     })
 }
