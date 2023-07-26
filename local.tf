4bae9091b3844843ba9fa204e497472862d357d1
cumberland-cloud <the.cumberland.cloud@gmail.com>
2023-07-26 13:56:20 -0400

    rejigger local

--- local.tf
+++ local.tf
@@ -1,8 +1,8 @@
 locals {
     ecr_name                        = "${var.repository.namespace}/${var.repository.name}"
-    encryption_configuration        = try({
+    encryption_configuration        = var.ecr.kms_key_arn != null ? {
         arn                         = var.ecr.kms_key_arn
-    }, {})
+    } : { }
     merge                           = var.ecr.policy != null
     policy                          = local.merge ? data.aws_iam_policy_document.merged[0].json : data.aws_iam_policy_document.unmerged.json
 }
