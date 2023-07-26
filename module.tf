6a0c2c8736565f0f145d7fc161985e3f75648def
cumberland-cloud <the.cumberland.cloud@gmail.com>
2023-07-26 18:54:56 -0400

    lock kms version

--- module.tf
+++ module.tf
@@ -1,6 +1,6 @@
 module "kms" {
     count           = local.conditions.provision_key ? 1 : 0
-    source          = "https://github.com/cumberland-cloud/modules-kms.git?ref=master"
+    source          = "https://github.com/cumberland-cloud/modules-kms.git?ref=v1.0.0"
 
     key             = {
         alias       = "${local.name}-ecr"
